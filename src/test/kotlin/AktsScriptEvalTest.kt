7a4af868158c885767a9b77a01997ee7df0eb63c
Gustav Munkby <grddev@gmail.com>
2018-11-14 10:15:18 +0100

    Add integration-style test for akts script
    This launches an inline akts script in a JVM scripting host.
    
    However, given that the AktsScript base class is defined in the compilation output, we need to inject the classpath containing the base class into the compilation. There are more high-level classpath manipulations that can be performed, but they seemed to match only the resources compilation output directory, which doesn't really help, so uses a not-so-nice low level hack.

---
+++ src/test/kotlin/AktsScriptEvalTest.kt
@@ -0,0 +1,52 @@
+package io.burt.akts
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.opentest4j.AssertionFailedError
+import kotlin.script.experimental.api.ResultValue
+import kotlin.script.experimental.api.ResultWithDiagnostics
+import kotlin.script.experimental.api.dependencies
+import kotlin.script.experimental.host.toScriptSource
+import kotlin.script.experimental.jvm.JvmDependency
+import kotlin.script.experimental.jvm.util.classpathFromClassloader
+import kotlin.script.experimental.jvmhost.BasicJvmScriptingHost
+import kotlin.script.experimental.jvmhost.createJvmCompilationConfigurationFromTemplate
+
+class AktsScriptEvalTest {
+  fun evalAkts(script: String): AktsScript {
+    val evaluationResult = BasicJvmScriptingHost().eval(
+      script.toScriptSource(),
+      createJvmCompilationConfigurationFromTemplate<AktsScript>() {
+        dependencies.append(
+          JvmDependency(
+            classpathFromClassloader(Thread.currentThread().contextClassLoader).orEmpty().filter {
+              it.path.contains("/akts/")
+            }
+          )
+        )
+      },
+      null
+    )
+
+    when (evaluationResult) {
+      is ResultWithDiagnostics.Success -> {
+        val returnValue = evaluationResult.value.returnValue as ResultValue.Value
+        return returnValue.value as AktsScript
+      }
+      is ResultWithDiagnostics.Failure -> {
+        println(evaluationResult.reports)
+        evaluationResult.reports.firstOrNull()?.exception?.printStackTrace()
+        throw AssertionFailedError("Expected akts script evaluation to succeed")
+      }
+    }
+  }
+
+  @Test
+  fun `#tests contains a container node for each registered describe block`() {
+    val instance = evalAkts("describe<Any>() {}; describe<String>() {}")
+    assertEquals(
+      listOf("Object", "String"),
+      instance.tests().map { it.displayName }.toList()
+    )
+  }
+}
